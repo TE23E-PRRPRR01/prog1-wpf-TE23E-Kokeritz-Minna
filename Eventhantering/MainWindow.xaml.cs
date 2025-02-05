4f780d9a91d63e6e02d8338c2b00fe4b638b8696
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2025-02-05 12:53:12 +0100

    eventhantering tränat

---
+++ Eventhantering/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eventhantering;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    
+    private void KlickOK(object sender, RoutedEventArgs e)
+    {
+       MessageBox.Show("Du klickade på OK");
+        string text = tbxText.Text;
+        MessageBox.Show($"Du skrev: {text}");
+        tbxResultat.Text = text;
+    }
+
+    private void KlickAvbryt(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Du klickade på Avbryt");
+        Close();
+    }
+}
