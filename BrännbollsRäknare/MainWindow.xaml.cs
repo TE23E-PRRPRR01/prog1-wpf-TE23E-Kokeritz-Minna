3ef8fb0f8889c11d781818bd01bd4b1fbb157007
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2025-03-10 14:52:14 +0100

    tränat på horizontell stackpanel + glabala variabler + prov uppgifter + klocka

---
+++ BrännbollsRäknare/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrännbollsRäknare;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    int poängUte = 0;
+    int poängInne = 0;
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void PoängRäknare(object sender, RoutedEventArgs e)
+    {
+        this.Background = Brushes.AliceBlue;
+
+        if (sender == Frivarv)
+        {
+            poängInne += 5;
+            tbxLagInne.Text = poängInne.ToString();
+
+            DateTime tid = DateTime.Now;
+
+            tbxHistorik.Text += $"{tid.ToString("HH:mm:ss")} Frivarv + 5 \n";
+        }
+
+        if (sender == Bränning)
+        {
+            poängUte += 2;
+            tbxLagUte.Text = poängUte.ToString();
+
+            DateTime tid = DateTime.Now;
+
+
+            tbxHistorik.Text += $"{tid.ToString("HH:mm:ss")} Bränning + 2 \n";
+        }
+
+        if (sender == Lyra)
+        {
+            poängUte += 3;
+            tbxLagUte.Text = poängUte.ToString();
+
+            DateTime tid = DateTime.Now;
+
+            tbxHistorik.Text += $"{tid.ToString("HH:mm:ss")} Lyra + 3 \n";
+        }
+
+        if (sender == Varv)
+        {
+            poängInne += 1;
+            tbxLagInne.Text = poängInne.ToString();
+
+            DateTime tid = DateTime.Now;
+
+            tbxHistorik.Text += $"{tid.ToString("HH:mm:ss")} Varv + 1 \n";
+        }
+    }
+}
---
+++ Prov_Övn/portoApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace portoApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickaBeräkna(object sender, RoutedEventArgs e)
+    {
+        bool heltal = int.TryParse(txbVikt.Text, out int vikt);
+        if (!heltal || vikt < 0){
+            txbVikt.Text = "Inte en riktig vikt i heltal: default 0g";
+            vikt = 0;
+        } 
+            
+        
+        if(vikt > 0 && vikt <= 50) lblResultat.Content = $"Brev som väger {vikt}g kostar 22kr (1 frimärke)";
+        else if(vikt > 50 && vikt <= 100) lblResultat.Content = $"Brev som väger {vikt}g kostar 44kr (2 frimärke)";
+        else if(vikt > 100 && vikt <= 250) lblResultat.Content = $"Brev som väger {vikt}g kostar 66kr (3 frimärke)";
+        else if(vikt > 250 && vikt <= 500) lblResultat.Content = $"Brev som väger {vikt}g kostar 88kr (4 frimärke)";
+        else if(vikt > 500 && vikt <= 1000) lblResultat.Content = $"Brev som väger {vikt}g kostar 132kr (6 frimärke)";
+        else if(vikt > 1000 && vikt <= 2000) lblResultat.Content = $"Brev som väger {vikt}g kostar 154kr (7 frimärke)";
+        else if(vikt > 2000) lblResultat.Content = $"Brev väger för mycket";
+        else lblResultat.Content = "Något blev fel";
+    }
+}
