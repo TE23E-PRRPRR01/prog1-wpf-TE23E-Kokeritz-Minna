578cbf8f36b6229f00fa9210946f6118bc2c498b
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2025-04-02 13:18:25 +0200

    provÖvn_upp6

---
+++ Prov_Övn2/upp_6/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace upp_6;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    double medelVärde = 0;
+    double summa = 0;
+    int antal = 0;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickAdd(object sender, RoutedEventArgs e)
+    {
+        if (!double.TryParse(txbTal.Text, out double tal)) MessageBox.Show("Felaktig inmatning");
+        else
+        {   
+            txbSumma.Text = $"{summa} + {tal} = {tal+summa}";
+            summa += tal;
+
+            antal ++; 
+            medelVärde =  summa/antal; 
+
+            txbMedel.Text = $"{summa} / {antal} = {medelVärde}";
+        }
+    }
+}
