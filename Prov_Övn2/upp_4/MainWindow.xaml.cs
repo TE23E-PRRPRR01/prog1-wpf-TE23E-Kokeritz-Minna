ac8288e6678e495a91dedd48951a55f9d28a78fb
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2025-04-02 13:32:33 +0200

    provÖvn_upp4 klar

---
+++ Prov_Övn2/upp_4/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace upp_4;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickÖppna(object sender, RoutedEventArgs e)
+    {
+        if(!File.Exists(txbFilNamn.Text)) MessageBox.Show("Fel: fil finns inte");
+        else  {
+            txbResult.Text = File.ReadAllText(txbFilNamn.Text);
+        }
+    }
+}
